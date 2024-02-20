cf6696b65987b82e0bcf431fafaf51cbf141c55d
Mit Donga <meetdonga5344@gmail.com>
2024-02-20 20:40:10 +0530

    Added test cases for APIs

---
+++ test/controllers/infos_controller_test.rb
@@ -0,0 +1,73 @@
+require "test_helper"
+
+class InfosControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @info = infos(:one)
+    @user = users(:eight)
+  end
+
+  test "should get index" do
+    get "/infos.json"
+    json_response = JSON.parse(response.body)
+
+    assert_response :ok
+    assert_equal Info.count, json_response["total_infos"]
+    assert_equal 1, json_response["page"]
+    assert_equal 10, json_response["per_page"]
+    assert_operator json_response["infos"].size, :>, 1
+  end
+
+  test "should create info" do
+    assert_difference("Info.count") do
+      post "/infos.json", params: { info: { age: @info.age, email: @info.email, phone: @info.phone, title: @info.title, user_id: @user.id } }
+    end
+    json_response = JSON.parse(response.body)
+
+    assert_response :created
+    assert_equal json_response["title"], @info.title
+    assert_equal json_response["email"], @info.email
+    assert_equal json_response["age"], @info.age
+    assert_equal json_response["user"]["id"], @user.id
+    assert_equal json_response["user"]["name"], @user.name
+  end
+
+  test "should show info" do
+    get "/infos/#{@info.id}.json"
+    json_response = JSON.parse(response.body)
+
+    assert_response :ok
+    assert_equal json_response["title"], @info.title
+    assert_equal json_response["email"], @info.email
+    assert_equal json_response["age"], @info.age
+    assert_equal json_response["user"]["name"], @info.user.name
+    assert_equal json_response["user"]["id"], @info.user.id
+  end
+
+  test "should show not found" do
+    get "/infos/1.json"
+
+    json_response = JSON.parse(response.body)
+    assert_response :not_found
+    assert_equal json_response["message"], "Info not found"
+  end
+
+  test "should update info" do
+    put "/infos/#{@info.id}.json", params: { info: { title: "MR.", age: 30, email: "testmail@example.com" } }
+    json_response = JSON.parse(response.body)
+
+    assert_response :ok
+    assert_equal json_response["title"], "MR."
+    assert_equal json_response["email"], "testmail@example.com"
+    assert_equal json_response["age"], 30
+  end
+
+  test "should destroy info" do
+    assert_difference("Info.count", -1) do
+      delete "/infos/#{@info.id}.json"
+    end
+    json_response = JSON.parse(response.body)
+
+    assert_response :ok
+    assert_equal json_response["message"], "Info was successfully deleted"
+  end
+end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,54 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get index" do
+    get "/users.json"
+    json_response = JSON.parse(response.body)
+
+    assert_response :ok
+    assert_equal User.count, json_response["total_users"]
+    assert_equal 1, json_response["page"]
+    assert_equal 10, json_response["per_page"]
+    assert_operator json_response["users"].size, :>, 1
+  end
+
+  test "should create user" do
+    assert_difference("User.count") do
+      post "/users.json", params: { user: { name: @user.name } }
+    end
+    json_response = JSON.parse(response.body)
+
+    assert_response :created
+    assert_equal json_response["name"], @user.name
+  end
+
+  test "should show user" do
+    get "/users/#{@user.id}.json"
+    json_response = JSON.parse(response.body)
+
+    assert_response :ok
+    assert_equal json_response["name"], @user.name
+  end
+
+  test "should update user" do
+    put "/users/#{@user.id}.json", params: { user: { name: "Meet Donga" } }
+    json_response = JSON.parse(response.body)
+
+    assert_response :ok
+    assert_equal json_response["name"], "Meet Donga"
+  end
+
+  test "should destroy user" do
+    assert_difference("User.count", -1) do
+      delete "/users/#{@user.id}.json"
+    end
+    json_response = JSON.parse(response.body)
+
+    assert_response :ok
+    assert_equal json_response["message"], "User was successfully deleted"
+  end
+end
